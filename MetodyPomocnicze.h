ad9d4121306bc5b14c2f3f15ff35b297e44b4ef3
Piotr Wasilewski <piotr.wasilewski.programista@gmail.com>
2022-04-26 16:52:46 -0500

    Divide KsiazkaAdresowa to smaller class

---
+++ MetodyPomocnicze.h
@@ -0,0 +1,20 @@
+#ifndef METODYPOMOCNICZE_H
+#define METODYPOMOCNICZE_H
+
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class MetodyPomocnicze
+{
+    public:
+    static string konwerjsaIntNaString(int liczba);
+};
+
+#endif
