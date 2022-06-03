7a2273b3e8ee5f1b2315292d79cab9b8a0f06910
Piotr Wasilewski <piotr.wasilewski.programista@gmail.com>
2022-06-02 20:54:23 -0500

    Make more cleare main menu

--- main.cpp
+++ main.cpp
@@ -12,43 +12,7 @@ int main()
     KsiazkaAdresowa ksiazkaAdresowa("Uzytkownicy.txt", "Adresaci.txt");
 
     ksiazkaAdresowa.wybierzOpcjeZMenuGlownego();
-    /*
-    ksiazkaAdresowa.wybierzOpcjeZMenuUzytkownika();
-
-    ksiazkaAdresowa.wypiszWszystkichUzytkownikow();
-
-    ksiazkaAdresowa.logowanieUzytkownika();
-
-    ksiazkaAdresowa.dodawanieNowegoAdresata();
-
-    ksiazkaAdresowa.wyswietlWszystkichAdresatow();
-
-    ksiazkaAdresowa.wylogujUzytkownika();
-    */
 
     return 0;
 
-}
-
-#include "AdresatMenager.h"
-int AdresatMenager_main(){
-
-AdresatMenager adresatMenager(2,"Adresaci.txt");
-adresatMenager.wczytanieAdresatowDoPamieci(2);
-adresatMenager.wyswietlWszystkichAdresatow();
-//adresatMenager.edytujAdresata();
-//adresatMenager.dodawanieNowegoAdresata(2);
-//adresatMenager.dodawanieNowegoAdresata(2);
-system("pause");
-//adresatMenager.wyswietlWszystkichAdresatow();
-//adresatMenager.usunAdresata();
-adresatMenager.wyszukajAdresatowPoImieniu();
-}
-
-#include "Menu.h"
-
-int Menu_main(){
-
-Menu menu;
-menu.wybierzOpcjeZMenuUzytkownika();
 }
